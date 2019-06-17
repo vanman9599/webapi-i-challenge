bf9b98543c7dca76af45ff935b1e193b9d6a0fb5
van jordan <vanjordan46@hotmail.com>
2019-06-17 15:40:44 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // implement your API here
+const express = require('express');
+const server = express();
+const db = require('./data/db.js');
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            success: false, 
+            error: "The users information could not be retrieved."
+        })
+    })
+})
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+       
+    // if(!userInfo.name || !userInfo.bio){
+    //     res.end();
+    //     res.status(400).json({
+    //         errorMessage: "Please provide name and bio for the user." 
+    //     })
+    // }
+
+    db.insert(userInfo)
+        .then(user => {
+            res.status(201).json({ success: true, user})
+            
+        })
+        .catch(err => {
+            res.status(500).json({
+                success: false, 
+                err
+            })
+        })
+})
+
+
+server.listen(4000, () => {
+    // this callback function run after the server starts successfully
+    console.log('\n*** Server Running on http://localhost:4000 ***\n')
+})
